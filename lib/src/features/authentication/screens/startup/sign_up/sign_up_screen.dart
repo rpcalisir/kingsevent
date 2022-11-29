f108ced16b65730cee5b1ed5ec9865bae3558b98
Recep Çalışır <rpcalisir@gmail.com>
2022-11-29 22:16:56 +0300

    Page navigations are added

---
+++ lib/src/features/authentication/screens/startup/sign_up/sign_up_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:kingsevent/src/common_widgets/form/form_header_widget.dart';
+import 'package:kingsevent/src/constants/sizes.dart';
+import 'package:kingsevent/src/constants/text_strings.dart';
+import 'package:kingsevent/src/features/authentication/screens/startup/sign_up/sign_up_screen_form_widget.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(rSignUp),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(rDefaultSize),
+          child: Column(
+            children: const [
+              FormHeaderWidget(),
+              SignUpScreenFormWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
